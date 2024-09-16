b329354ba94f494481266f65c6bcf51c806498a5
Chris Vasiladiotis <cvassiladiotis@gmail.com>
2024-09-16 14:28:33 +0100

    Add plan

--- src/ch/planrepo.ts
+++ src/ch/planrepo.ts
@@ -35,6 +35,12 @@ export interface TrainingPlan {
 
 export const availablePlans: AvailablePlan[] = [
   {
+    url: Config.plansPath + "frr_bt_02.json",
+    id: "frr_bt_02",
+    name: "Faster Road Racing: Base Training 2",
+    type: "Base",
+  },
+  {
     url: Config.plansPath + "c25k.json",
     id: "c25k",
     name: "Couch to 5K",
