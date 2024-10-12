013b84c464f676aeffc96c657123e1c0587a7898
nanreh <hernan@defy.org>
2024-10-12 07:48:32 -0700

    prettier

--- src/ch/localize.ts
+++ src/ch/localize.ts
@@ -3,16 +3,14 @@ import { Units } from "types/app";
 const formatter = new Intl.DateTimeFormat(navigator.language);
 
 // Locale-appropriate date format based on current language.
-export function format(
-    date: Date
-): string {
-    return formatter.format(date);
-};
+export function format(date: Date): string {
+  return formatter.format(date);
+}
 
 // Guess the user's preferred units of measure based on the current language. Fuzzy at best.
 export function getLocaleUnits(): Units {
-    if (navigator.language === "en" || navigator.language === "en-US") {
-        return "mi";
-    }
-    return "km";
+  if (navigator.language === "en" || navigator.language === "en-US") {
+    return "mi";
+  }
+  return "km";
 }
